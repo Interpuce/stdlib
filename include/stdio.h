5069390c1cdd7fe4bbb2220b05796217dfcee744
Gorciu <gorciuyt@gmail.com>
2024-12-05 17:05:26 +0100

    more spaces

--- include/stdio.h
+++ include/stdio.h
@@ -7,10 +7,10 @@ extern void printstr(const char *str, uint8_t color);
 uint8_t current_color = 0x07;
 
 void print(const char *str) {
-  printstr(str, current_color);
+    printstr(str, current_color);
 }
 
 void set_color(uint8_t color) {
-  printstr("", color);
-  current_color = color;
+    printstr("", color);
+    current_color = color;
 }
