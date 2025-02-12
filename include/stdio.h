42d435253f721e99c313ae4f5c8482161fa329ed
Gorciu <gorciuyt@gmail.com>
2025-02-12 12:48:21 +0100

    use syscalls

--- include/stdio.h
+++ include/stdio.h
@@ -2,18 +2,15 @@
 
 #include <stdint.h>
 #include <string.h>
-
-extern void printstr(const char *str, uint8_t color);
-void read_str(char *buffer, uint16_t max_length, uint8_t secret, uint8_t color);
-uint8_t read_yn(char *text, uint8_t color);
+#include <sys/syscall.h>
 
 uint8_t current_color = 0x07;
 
 void print(const char *str) {
-    printstr(str, current_color);
+    _print(str, current_color);
 }
 
 void set_color(uint8_t color) {
-    printstr("", color);
+    _print("", color);
     current_color = color;
 }
